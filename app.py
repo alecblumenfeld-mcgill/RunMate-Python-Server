05ce841398e5345ef08f46736cbdb58e24c228ca
alec harmon <alec.blumenfeld@mail.mcgill.ca>
2016-03-31 18:44:43 -0400

    added yoooo

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ Flask Documentation:     http://flask.pocoo.org/docs/
 Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 
-This file creates your application.
 """
 
 import os
